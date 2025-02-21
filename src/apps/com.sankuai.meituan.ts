220dcf7fc71fc39f83a3e5fadc6c3d858496dd0e
ccciyo <ccciyo@gmail.com>
2025-02-21 11:13:36 +0800

    com.sankuai.meituan

---
+++ src/apps/com.sankuai.meituan.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.sankuai.meituan',
+  name: '美团',
+  groups: [
+    {
+      key: 0,
+      name: '直播',
+      fastQuery: true,
+      activityIds: ['com.sankuai.waimai.business.restaurant.poicontainer.WMRestaurantActivity'],
+      rules: [
+        {
+          key: 0,
+          matches: [
+            'ImageView + @TextView[text="正在讲解"]',
+          ],
+        },
+      ],
+    },
+  ],
+});
